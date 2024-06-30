a4a1602a3e91d5f95a476d53032ff18eaaee551e
kabindrr <chaina_kabindra@yahoo.com>
2024-07-01 07:20:13 +1000

    login failed

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUser } from "../model/userModal.js";
+import { addUser, getUser } from "../model/userModal.js";
 
 export const userRouter = express.Router();
 
@@ -10,5 +10,14 @@ userRouter.post("/", async (req, res) => {
   res.json({
     result,
     message: "Following user has been added to the database",
+  });
+});
+
+userRouter.get("/", async (req, res) => {
+  const result = await getUser(req.body);
+
+  res.json({
+    result,
+    message: "Here is the user you are looking for",
   });
 });
